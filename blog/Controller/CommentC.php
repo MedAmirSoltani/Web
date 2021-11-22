e6945c65d68cbd704a893d82cdd3ee7d7c9f3839
Too lazy To name It <2lazy2nameit@gmail.com>
2021-11-22 20:03:41 +0100

    SEC CRUD INIT

---
+++ blog/Controller/CommentC.php
@@ -0,0 +1,104 @@
+<?php
+require_once "../assets/ASFO/utilis/Config.php";
+require_once '../Model/Comment.php';
+class CommentC
+{
+
+
+
+    function ShowComment($idp)
+    {
+        $requete = "select * from comment Where Idpost=:idp";
+        $config = config::getConnexion();
+        try {
+            $querry = $config->prepare($requete);
+            $querry->execute(['idp' => $idp]);
+            $result = $querry->fetchAll();
+            return $result;
+        } catch (PDOException $th) {
+            $th->getMessage();
+        }
+    }
+    function GetCommentbyID($idc)
+    {
+        $requete = "select * from comment where Idcomment=:idc";
+        $config = config::getConnexion();
+        try {
+            $querry = $config->prepare($requete);
+            $querry->execute(
+                [
+                    'idc' => $idc
+                ]
+            );
+            $result = $querry->fetch();
+            return $result;
+        } catch (PDOException $th) {
+            $th->getMessage();
+        }
+    }
+
+    function AddComment($Comment)
+    {
+
+        $config = config::getConnexion();
+        try {
+            // $this->uploadFile();
+            $querry = $config->prepare('
+            INSERT INTO comment 
+            (Idpost,Comment_text,Idcomment,Date_Comment)
+            VALUES
+            (:Idpost,:Comment_text,:Idcomment,:Date_Comment)
+            ');
+            $querry->execute([
+                'Idpost' => $Comment->getIdpost(),
+                'Comment_text' => $Comment->getComment_text(),
+                'Idcomment' => $Comment->getIdcomment(),
+                'Date_Comment' => $Comment->getDate_Comment(),
+
+
+
+
+            ]);
+        } catch (PDOException $th) {
+            $th->getMessage();
+        }
+    }
+    function UpdateComment($Comment)
+    {
+        $config = config::getConnexion();
+        try {
+            $querry = $config->prepare('
+            UPDATE comment SET
+            Title=:Title, Comment_text=:Comment_text,Idcomment=:Idcomment, Date_Comment=:Date_Comment
+
+            where Idcomment=:Idcomment
+            ');
+            $querry->execute([
+                'Idpost' => $Comment->getIdpost(),
+                'Comment_text' => $Comment->getComment_text(),
+                'Idcomment' => $Comment->getIdcomment(),
+                'Date_Comment' => $Comment->getDate_Comment(),
+
+            ]);
+        } catch (PDOException $th) {
+            $th->getMessage();
+        }
+    }
+
+    function RemoveComment($idc)
+    {
+        $config = config::getConnexion();
+        try {
+            $querry = $config->prepare('
+            DELETE FROM comment WHERE Idcomment =:idc
+            ');
+            $querry->execute([
+                'idc' => $idc
+            ]);
+        } catch (PDOException $th) {
+            $th->getMessage();
+        }
+        header("Refresh:0");
+    }
+}
+?>
---
+++ blog/Model/Comment.php
@@ -0,0 +1,59 @@
+<?php
+class Comment
+{
+  private $Idpost;
+  private $Comment_text;
+  private $Idcomment;
+  private $Date_Comment;
+
+
+
+
+
+
+
+  function __construct($idpo, $comment_text, $idcomment, $date_Comment, $description)
+  {
+    $this->Idpost = $idpo;
+    $this->Comment_text = $comment_text;
+    $this->Idcomment = $idcomment;
+    $this->Date_Comment = $date_Comment;
+    $this->Description = $description;
+  }
+
+  function setIdpost(string $Idpost)
+  {
+    $this->Idpost = $Idpost;
+  }
+  function setDate_Comment(string $Date_Comment)
+  {
+    $this->Date_Comment = $Date_Comment;
+  }
+  function setIdcomment(string $Idcomment)
+  {
+    $this->Idcomment = $Idcomment;
+  }
+  function setComment_text(string $Comment_text)
+  {
+    $this->Comment_text = $Comment_text;
+  }
+
+
+  function getIdpost()
+  {
+    return $this->Idpost;
+  }
+  function getDate_Comment()
+  {
+    return $this->Date_Comment;
+  }
+  function getIdcomment()
+  {
+    return $this->Idcomment;
+  }
+  function getComment_text()
+  {
+    return $this->Comment_text;
+  }
+}
+?>
