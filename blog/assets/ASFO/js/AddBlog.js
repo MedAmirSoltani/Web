e566a06192f7de80eabbde0aedfb0f4b80fb2fc6
Too lazy To name It <2lazy2nameit@gmail.com>
2021-11-29 13:40:15 +0100

    nrigel fil alert

--- blog/assets/ASFO/js/AddBlog.js
+++ blog/assets/ASFO/js/AddBlog.js
@@ -33,9 +33,9 @@ function NowDate() {
 
 function CheckTitle() {
     var Title = document.getElementById("Title").value;
+    var warn = document.getElementById("wrtitle");
     if (Title === "") {
-
-        alert("Title is empty");
+        warn.innerHTML = '<div class="alert alert-warning alert-dismissible d-flex align-items-center fade show"><i class="bi-exclamation-triangle-fill"></i><strong class="mx-2">Warning!</strong> There was a problem with your network connection.<button type="button" class="btn-close" data-bs-dismiss="alert"></button></div>';
 
         return false;
 
