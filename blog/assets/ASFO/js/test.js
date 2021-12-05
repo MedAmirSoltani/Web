e0856417cc953a75d2b9b66a81a9b9ba77bf9759
Too lazy To name It <2lazy2nameit@gmail.com>
2021-12-05 15:46:20 +0100

    fixin backoffice

--- blog/assets/ASFO/js/test.js
+++
@@ -1,9 +0,0 @@
-alert("BARA IL FUNCTION1");
-function affich() {
-    
-   let nav=document.getElementById("Tables");
-   
-   if(nav.style.display=="none")
-   nav.style.display="block";
-   else nav.style.display="none";
-}
