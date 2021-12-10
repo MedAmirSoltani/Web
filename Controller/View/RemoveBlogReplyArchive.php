1aa714b28ecf1f033be82bc011c1f06fb9603ddd
djapa-maker <yesmine.guesmi@esprit.tn>
2021-12-10 21:16:05 +0100

    icon

--- Controller/View/RemoveBlogReplyArchive.php
+++
@@ -1,7 +0,0 @@
-<?php
-    require '../Controller/ReplyC.php';
-
-    $ReplyC = new ReplyC();
-    $ReplyC->RemoveReplyArchive($_GET["Idreply"]);
-    header('Location:AdminViewBlogHomeArchive.php');
-?>
--- Controller/View/supprimerutilisateur.php
+++
@@ -1,7 +0,0 @@
-<?php
-    require '../Controller/utilisateurC.php';
-
-    $utilisateurC = new utilisateurC();
-    $utilisateurC->supprimerutilisateur($_GET['ID_utilisateur']);
-    header('Location:afficherutilisateur.php');
-?>
