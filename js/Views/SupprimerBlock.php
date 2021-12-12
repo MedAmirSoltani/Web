a247f8d40018e1fb903162a30c22b942f820c467
Too lazy To name It <2lazy2nameit@gmail.com>
2021-12-12 19:09:45 +0100

    fixin

--- js/Views/SupprimerBlock.php
+++
@@ -1,6 +0,0 @@
-<?php
-	include '../Controller/BlockC.php';
-	$BlockC = new BlockC();
-	$BlockC->supprimerblock($_GET["Id"]);
-	header('Location:ListeBlocks.php');
-?>
--- js/Views/SupprimerSalles.php
+++
@@ -1,6 +0,0 @@
-<?php
-	include '../Controller/SallesC.php';
-	$SallesC = new SallesC();
-	$SallesC->supprimerSalles($_GET["Id"]);
-	header('Location:ListeSalles.php');
-?>
