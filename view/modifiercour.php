a3d52170123942545aff40f5413a88a15aa41f88
Too lazy To name It <2lazy2nameit@gmail.com>
2021-12-14 00:16:18 +0100

    Lezmek tfasekh sea eli mawjoudin fi tab salle khater el cle primaire hedhi cle etranger fi tabl sale

--- view/modifiercour.php
+++ view/modifiercour.php
@@ -1,59 +1,59 @@
 <?php
 session_start();
-    require_once     '../Controller/courC.php';
-    require_once '../Model/cour.php' ;
-    include_once     '../Controller/utilisateurC.php';
-    require_once     '../Controller/matiereC.php';
-    $courC = new courC();
-    $userC1=new utilisateurC();
-    $conn=$userC1->getutilisateurbyID($_SESSION['a']);
-    if (isset($_POST['idcour'] ) && isset($_POST['ncour']  ) && isset($_POST['file']  )&& isset($_POST['idmatiere']  )) 
-    {
-        echo $_POST['idcour'] ;
-            $cour = new cour($_POST['idcour'] , $_POST['ncour'], $_POST['file'], $_POST['idmatiere'] );
-            $courC->modifiercour($cour);
-            $id=$_POST['idmatiere'];
-            header("Location:affichercour.php?idmatiere=$id");
-    }else
-    {
-      $matiereC = new matiereC();
-      $resultats = $matiereC -> affichermatiere();
-        $a = $courC->getcourbyID($_GET['idcour']) ;
-    }
+require_once     '../Controller/courC.php';
+require_once '../Model/cour.php';
+include_once     '../Controller/utilisateurC.php';
+require_once     '../Controller/matiereC.php';
+$courC = new courC();
+$userC1 = new utilisateurC();
+$conn = $userC1->getutilisateurbyID($_SESSION['a']);
+if (isset($_POST['idcour']) && isset($_POST['ncour']) && isset($_POST['file']) && isset($_POST['idmatiere'])) {
+  echo $_POST['idcour'];
+  $cour = new cour($_POST['idcour'], $_POST['ncour'], $_POST['file'], $_POST['idmatiere']);
+  $courC->modifiercour($cour);
+  $id = $_POST['idmatiere'];
+  header("Location:affichercour.php?idmatiere=$id");
+} else {
+  $matiereC = new matiereC();
+  $resultats = $matiereC->affichermatiere();
+  $a = $courC->getcourbyID($_GET['idcour']);
+}
 
 
 
 ?>
 <html class="loading" lang="en" data-textdirection="ltr">
-  <head>
-    <meta http-equiv="Content-Type" content="text/html; charset=UTF-8">
-    <meta http-equiv="X-UA-Compatible" content="IE=edge">
-    <meta name="viewport" content="width=device-width, initial-scale=1.0, user-scalable=0, minimal-ui">
-    <meta name="description" content="Chameleon Admin is a modern Bootstrap 4 webapp &amp; admin dashboard html template with a large number of components, elegant design, clean and organized code.">
-    <meta name="keywords" content="admin template, Chameleon admin template, dashboard template, gradient admin template, responsive admin template, webapp, eCommerce dashboard, analytic dashboard">
-    <meta name="author" content="ThemeSelect">
-    <title>Hogwarts</title>
-    <link rel="apple-touch-icon" href="../Assets/theme-assets/images/ico/apple-icon-120.png">
-   
-    <link href="https://fonts.googleapis.com/css?family=Muli:300,300i,400,400i,600,600i,700,700i%7CComfortaa:300,400,700" rel="stylesheet">
-    <link href="https://maxcdn.icons8.com/fonts/line-awesome/1.1/css/line-awesome.min.css" rel="stylesheet">
-    <!-- BEGIN VENDOR CSS-->
-    <link rel="stylesheet" type="text/css" href="../Assets/theme-assets/css/vendors.css">
-    <!-- END VENDOR CSS-->
-    <!-- BEGIN CHAMELEON  CSS-->
-    <link rel="stylesheet" type="text/css" href="../Assets/theme-assets/css/app-lite.css">
-    <!-- END CHAMELEON  CSS-->
-    <!-- BEGIN Page Level CSS-->
-    <link rel="stylesheet" type="text/css" href="../Assets/theme-assets/css/core/menu/menu-types/vertical-menu.css">
-    <link rel="stylesheet" type="text/css" href="../Assets/theme-assets/css/core/colors/palette-gradient.css">
-    <!-- END Page Level CSS-->
-    <!-- BEGIN Custom CSS-->
-    <!-- END Custom CSS-->
-  </head>
-  <body class="vertical-layout vertical-menu 2-columns   menu-expanded fixed-navbar" data-open="click" data-menu="vertical-menu" data-color="bg-gradient-x-purple-blue" data-col="2-columns">
 
-    <!-- fixed-top-->
-    <nav class="header-navbar navbar-expand-md navbar navbar-with-menu navbar-without-dd-arrow fixed-top navbar-semi-light">
+<head>
+  <meta http-equiv="Content-Type" content="text/html; charset=UTF-8">
+  <meta http-equiv="X-UA-Compatible" content="IE=edge">
+  <meta name="viewport" content="width=device-width, initial-scale=1.0, user-scalable=0, minimal-ui">
+  <meta name="description" content="Chameleon Admin is a modern Bootstrap 4 webapp &amp; admin dashboard html template with a large number of components, elegant design, clean and organized code.">
+  <meta name="keywords" content="admin template, Chameleon admin template, dashboard template, gradient admin template, responsive admin template, webapp, eCommerce dashboard, analytic dashboard">
+  <meta name="author" content="ThemeSelect">
+  <title>Hogwarts</title>
+  <link rel="apple-touch-icon" href="../Assets/theme-assets/images/ico/apple-icon-120.png">
+
+  <link href="https://fonts.googleapis.com/css?family=Muli:300,300i,400,400i,600,600i,700,700i%7CComfortaa:300,400,700" rel="stylesheet">
+  <link href="https://maxcdn.icons8.com/fonts/line-awesome/1.1/css/line-awesome.min.css" rel="stylesheet">
+  <!-- BEGIN VENDOR CSS-->
+  <link rel="stylesheet" type="text/css" href="../Assets/theme-assets/css/vendors.css">
+  <!-- END VENDOR CSS-->
+  <!-- BEGIN CHAMELEON  CSS-->
+  <link rel="stylesheet" type="text/css" href="../Assets/theme-assets/css/app-lite.css">
+  <!-- END CHAMELEON  CSS-->
+  <!-- BEGIN Page Level CSS-->
+  <link rel="stylesheet" type="text/css" href="../Assets/theme-assets/css/core/menu/menu-types/vertical-menu.css">
+  <link rel="stylesheet" type="text/css" href="../Assets/theme-assets/css/core/colors/palette-gradient.css">
+  <!-- END Page Level CSS-->
+  <!-- BEGIN Custom CSS-->
+  <!-- END Custom CSS-->
+</head>
+
+<body class="vertical-layout vertical-menu 2-columns   menu-expanded fixed-navbar" data-open="click" data-menu="vertical-menu" data-color="bg-gradient-x-purple-blue" data-col="2-columns">
+
+  <!-- fixed-top-->
+  <nav class="header-navbar navbar-expand-md navbar navbar-with-menu navbar-without-dd-arrow fixed-top navbar-semi-light">
     <div class="navbar-wrapper">
       <div class="navbar-container content">
         <div class="collapse navbar-collapse show" id="navbar-mobile">
@@ -75,28 +75,29 @@ session_start();
               </ul>
             </li>
           </ul>
-         
+
           <ul class="nav navbar-nav float-right">
-          
-          <?php if (empty($conn['profilpicture']))
-                                                  {
-                                                    echo '<img src="../Assets/uploads/unknown.png" onclick="pictureclick()" id="profildisplay" style="width:100%; height:50px;border-radius:10%; display:block;"/>';
-                                                   }
-                                                
-                                                 ?>
-                                                  <img <?php if (empty($conn['profilpicture'])){ echo 'style="display:none;"'; } ?>  id="profildisplay" style="width:25%; height:35px; float:left;margin:0 10px 0 250px; border-radius:50%; display:block;" src="../Assets/uploads/<?php echo $conn['profilpicture'] ?>">  
-                                                 
-                                      
+
+            <?php if (empty($conn['profilpicture'])) {
+              echo '<img src="../Assets/uploads/unknown.png" onclick="pictureclick()" id="profildisplay" style="width:100%; height:50px;border-radius:10%; display:block;"/>';
+            }
+
+            ?>
+            <img <?php if (empty($conn['profilpicture'])) {
+                    echo 'style="display:none;"';
+                  } ?> id="profildisplay" style="width:25%; height:35px; float:left;margin:0 10px 0 250px; border-radius:50%; display:block;" src="../Assets/uploads/<?php echo $conn['profilpicture'] ?>">
+
+
           </ul>
           <ul class="nav navbar-nav float-right">
-           
-           
-              <div class="dropdown-menu dropdown-menu-right">
-                <div class="arrow_box_right"><a class="dropdown-item" href="#"><span class="avatar avatar-online"><img src="../Assets/theme-assets/images/portrait/small/avatar-s-19.png" alt="avatar"><span class="user-name text-bold-700 ml-1">John Doe</span></span></a>
-                  <div class="dropdown-divider"></div><a class="dropdown-item" href="#"><i class="ft-user"></i> Edit Profile</a><a class="dropdown-item" href="#"><i class="ft-mail"></i> My Inbox</a><a class="dropdown-item" href="#"><i class="ft-check-square"></i> Task</a><a class="dropdown-item" href="#"><i class="ft-message-square"></i> Chats</a>
-                  <div class="dropdown-divider"></div><a class="dropdown-item" href="#"><i class="ft-power"></i> Logout</a>
-                </div>
+
+
+            <div class="dropdown-menu dropdown-menu-right">
+              <div class="arrow_box_right"><a class="dropdown-item" href="#"><span class="avatar avatar-online"><img src="../Assets/theme-assets/images/portrait/small/avatar-s-19.png" alt="avatar"><span class="user-name text-bold-700 ml-1">John Doe</span></span></a>
+                <div class="dropdown-divider"></div><a class="dropdown-item" href="#"><i class="ft-user"></i> Edit Profile</a><a class="dropdown-item" href="#"><i class="ft-mail"></i> My Inbox</a><a class="dropdown-item" href="#"><i class="ft-check-square"></i> Task</a><a class="dropdown-item" href="#"><i class="ft-message-square"></i> Chats</a>
+                <div class="dropdown-divider"></div><a class="dropdown-item" href="#"><i class="ft-power"></i> Logout</a>
               </div>
+            </div>
             </li>
           </ul>
         </div>
@@ -104,10 +105,10 @@ session_start();
     </div>
   </nav>
 
-    <!-- ////////////////////////////////////////////////////////////////////////////-->
+  <!-- ////////////////////////////////////////////////////////////////////////////-->
 
 
-    <div class="main-menu menu-fixed menu-light menu-accordion    menu-shadow " data-scroll-to-active="true" data-img="../Assets/theme-assets/images/backgrounds/02.jpg">
+  <div class="main-menu menu-fixed menu-light menu-accordion    menu-shadow " data-scroll-to-active="true" data-img="../Assets/theme-assets/images/backgrounds/02.jpg">
     <div class="navbar-header">
       <ul class="nav navbar-nav flex-row">
         <li class="nav-item mr-auto"><a class="navbar-brand" href="index.html"><img class="brand-logo" alt="Chameleon admin logo" src="../Assets/Images/logo.PNG" />
@@ -119,9 +120,9 @@ session_start();
     </div>
     <div class="main-menu-content">
       <ul class="navigation navigation-main" id="main-menu-navigation" data-menu="menu-navigation">
-        <li class=" active"><a href="afficherutilisateur.php"><i class="ft-home"></i><span class="menu-title" data-i18n="">Users</span></a>
+      <li class="nav-item"><a href="afficherutilisateur.php"><i class="ft-home"></i><span class="menu-title" data-i18n="">Users</span></a>
         </li>
-        <li class=" nav-item"><a href="affichermatiere.php"><i class="ft-book"></i><span class="menu-title" data-i18n="">Subjects</span></a>
+        <li class="active"><a href="affichermatiere.php"><i class="ft-book"></i><span class="menu-title" data-i18n="">Subjects</span></a>
         </li>
         <li class="nav-item"><a href="afficherclub.php"><i class="ft-credit-card"></i><span class="menu-title" data-i18n="">Clubs</span></a>
         </li>
@@ -140,108 +141,110 @@ session_start();
         </li>
         <li class="nav-item"><a href="afficherRegistre_appel.php"><i class="ft-credit-card"></i><span class="menu-title" data-i18n="">Absence</span></a>
         </li>
-       
 
       </ul>
     </div>
     <div class="navigation-background"></div>
   </div>
-    <div class="app-content content">
-      <div class="content-wrapper">
-        <div class="content-wrapper-before"></div>
-        <div class="content-header row">
-          <div class="content-header-left col-md-4 col-12 mb-2">
-        
+  <div class="app-content content">
+    <div class="content-wrapper">
+      <div class="content-wrapper-before"></div>
+      <div class="content-header row">
+        <div class="content-header-left col-md-4 col-12 mb-2">
+
+        </div>
+        <div class="content-header-right col-md-8 col-12">
+          <div class="breadcrumbs-top float-md-right">
+            <div class="breadcrumb-wrapper mr-1">
+              <ol class="breadcrumb">
+                <li class="breadcrumb-item"><a href="profiladmin.php">Home</a>
+                </li>
+
+                </li>
+              </ol>
+            </div>
           </div>
-          <div class="content-header-right col-md-8 col-12">
-            <div class="breadcrumbs-top float-md-right">
-              <div class="breadcrumb-wrapper mr-1">
-                <ol class="breadcrumb">
-                <li class="breadcrumb-item"><a href="profiladmin.php">Home</a> 
-                  </li>
-        
-                  </li>
-                </ol>
+        </div>
+      </div>
+      <div class="content-body">
+        <!-- Basic Tables start -->
+        <div class="row">
+          <div class="col-12">
+            <div class="card">
+              <div class="card-header">
+                <h4 class="card-title">update</h4>
+                <a class="heading-elements-toggle"><i class="la la-ellipsis-v font-medium-3"></i></a>
+                <div class="heading-elements">
+                  <ul class="list-inline mb-0">
+                    <li><a data-action="collapse"><i class="ft-minus"></i></a></li>
+                    <li><a data-action="reload"><i class="ft-rotate-cw"></i></a></li>
+                    <li><a data-action="expand"><i class="ft-maximize"></i></a></li>
+                    <li><a data-action="close"><i class="ft-x"></i></a></li>
+                  </ul>
+                </div>
+              </div>
+              <div class="card-content collapse show">
+                <div class="card-body">
+
+                  <div class="table-responsive">
+                    <form action="" method="POST">
+                      <table class="table" border="1" align="center">
+                        <tr>
+                          <td>
+                            <label for="idcour">idcour:
+                            </label>
+                          </td>
+                          <td><input type="number" name="idcour" id="idcour" maxlength="20" value="<?php echo $a['idcour']; ?>" readonly></td>
+                        </tr>
+                        <tr>
+                          <td>
+                            <label for="ncour">cour:
+                            </label>
+                          </td>
+                          <td><input type="text" value="<?php echo $a['ncour']; ?>" name="ncour" id="ncour" maxlength="20"></td>
+                        </tr>
+
+                        <tr>
+                          <td>
+                            <label for="file">file:
+                            </label>
+                          </td>
+                          <td><input type="file" value="<?php echo $a['file']; ?>" name="file" id="file" maxlength="20"></td>
+                        </tr>
+                        <tr>
+                          <td>
+                            <label for="idmatiere">idmatiere:
+                            </label>
+
+
+                          <td><select name="idmatiere" id="idmatiere" required>
+                              <option value="">--Please choose an option--</option>
+                              <?php foreach ($resultats as $value) {
+                              ?>
+                                <option value="<?php echo ($value["idmatiere"]) ?>"> <?php echo ($value["titre"]) ?></option>
+
+                              <?php } ?>
+                          </td>
+                          </select>
+
+                        </tr>
+                        <tr>
+                          <td>
+                            <input type="submit" value="Modifier">
+                          </td>
+                          <td>
+                            <input type="reset" value="Annuler">
+                          </td>
+                        </tr>
+                      </table>
+                    </form>
+                  </div>
+                </div>
               </div>
             </div>
           </div>
         </div>
-        <div class="content-body"><!-- Basic Tables start -->
-<div class="row">
-	<div class="col-12">
-		<div class="card">
-			<div class="card-header">
-				<h4 class="card-title">update</h4>
-				<a class="heading-elements-toggle"><i class="la la-ellipsis-v font-medium-3"></i></a>
-				<div class="heading-elements">
-					<ul class="list-inline mb-0">
-						<li><a data-action="collapse"><i class="ft-minus"></i></a></li>
-						<li><a data-action="reload"><i class="ft-rotate-cw"></i></a></li>
-						<li><a data-action="expand"><i class="ft-maximize"></i></a></li>
-						<li><a data-action="close"><i class="ft-x"></i></a></li>
-					</ul>
-				</div>
-			</div>
-			<div class="card-content collapse show">
-				<div class="card-body">
 
-					<div class="table-responsive">
-						<form action="" method="POST">
-							<table class="table" border="1" align="center">
-								<tr>
-									<td>
-										<label for="idcour">idcour:
-										</label>
-									</td>
-									<td><input type="number" name="idcour" id="idcour" maxlength="20" value="<?php echo $a['idcour'];?>"  readonly></td>
-								</tr>
-								<tr>
-									<td>
-										<label for="ncour">cour:
-										</label>
-									</td>
-									<td><input type="text" value="<?php echo $a['ncour'];?>" name="ncour" id="ncour" maxlength="20"></td>
-								</tr>
+</body>
 
-                <tr>
-									<td>
-										<label for="file">file:
-										</label>
-									</td>
-									<td><input type="file" value="<?php echo $a['file'];?>" name="file" id="file" maxlength="20"></td>
-								</tr>
-                <tr>
-									<td>
-                <label for="idmatiere">idmatiere:
-                </label>
-          
-              
-              <td><select name="idmatiere" id="idmatiere" required>
-                  <option value="">--Please choose an option--</option>
-                    <?php foreach ($resultats as $value) {
-                      ?>
-    <option value="<?php echo($value["idmatiere"])?>"> <?php echo($value["titre"])?></option>
-
-  <?php }?>
-</td>
-</select>
-
-</tr>		<tr>
-									<td>
-										<input type="submit" value="Modifier"> 
-									</td>
-									<td>
-										<input type="reset" value="Annuler" >
-									</td>
-								</tr>
-							</table>
-						</form>
-					</div>
-				</div>
-			</div>
-		</div>
-	</div>
-</div>
-
-  </body>
 </html>
